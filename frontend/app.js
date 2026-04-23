193e19769999389b5e08386792fdafcbc821a824
li33iez <oluwaseyiabolaji111@gmail.com>
2026-04-23 17:18:34 +0000

    port fixes

--- frontend/app.js
+++ frontend/app.js
@@ -3,22 +3,16 @@ const axios = require('axios');
 const path = require('path');
 const app = express();
 
-// no hardcoded, port and URL are in the env
 const API_URL = process.env.API_URL;
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 if (!API_URL) {
   throw new Error("API_URL is not defined in environment variables");
 }
 
-if (!PORT) {
-  throw new Error("PORT is not defined in environment variables");
-}
-
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'views')));
 
-// Added for Docker HEALTHCHECK
 app.get('/', (req, res) => {
   res.status(200).send('OK');
 });
@@ -41,7 +35,6 @@ app.get('/status/:id', async (req, res) => {
   }
 });
 
-// Added '0.0.0.0' so container is reachable
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Frontend running on port ${PORT}`);
-}); // Added missing closing
+});
